e7b887a2a4b556916621a13e759c335aaf43fad5
ysys <1243933010@qq.com>
2024-08-13 18:18:40 +0800

    feat():修改正式环境图片前缀

--- config/config.pro.ts
+++ config/config.pro.ts
@@ -1,6 +1,6 @@
 export default {
     define:{
-        UPLOAD_IMG_URL:'',
+        UPLOAD_IMG_URL:'http://127.0.0.1:3006',
         IMG_URL:'',
         REQUEST_URL:'',
         REQUEST_URL1:'http://localhost:3006/'
